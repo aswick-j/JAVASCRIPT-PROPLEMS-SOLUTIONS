a221eb712cc4088bf8100a16cf6af795fa930264
aswick <aswickjothi27@gmail.com>
2023-04-21 17:10:50 +0530

    NumberSwapping

--- NumberSwap.js
+++ NumberSwap.js
@@ -1,3 +1,15 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 //output [1,9,2,8,3,7,4,6,5]
+
+const swap = [];
+
+for (let i = 0; i < arr.length; i++) {
+  if (i % 2 === 0) {
+    swap.push(arr[Math.floor(i / 2)]);
+  } else {
+    swap.push(arr[arr.length - Math.floor(i / 2) - 1]);
+  }
+}
+
+console.log(swap);
