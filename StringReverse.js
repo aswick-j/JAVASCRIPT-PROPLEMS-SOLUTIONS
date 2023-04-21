c1de8f94e9c0d856cfd3c53bac60c3fefd46b8f8
aswick <aswickjothi27@gmail.com>
2023-04-21 17:19:11 +0530

    String Reverse

---
+++ StringReverse.js
@@ -0,0 +1,10 @@
+const string = "Hi Aswick";
+
+//Normal
+const reverseString = string.split("").reverse("").join("");
+//Converting to Lower Case
+// const reverseString = string.toLowerCase().split("").reverse("").join("");
+//Converting to Upper Case
+// const reverseString = string.toUpperCase().split("").reverse("").join("");
+
+console.log(reverseString);
